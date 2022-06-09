f4c338d8d934ca4d51cd9dae9f7ff842b1e4d8b6
zhangr4 <ran.zhang863@hotmail.com>
2022-06-09 22:35:34 +0800

    update pdf.sh with args

--- pdf.sh
+++ pdf.sh
@@ -1,12 +1,21 @@
-#! /usr/bin/env bash
+#! /usr/bin/sh
 
-find . -name "*.pdf" -print0 | xargs -0 rm -f
+# find . -name "*.pdf" -print0 | xargs -0 rm -f
 
-for f in resume_markdown/*.md; 
-do pandoc "$f" -s -f markdown+yaml_metadata_block \
---template resume_template/resume_template.latex -o "${f%.md}.pdf"; 
-done
+# $# indicate the length of variables 
+if [ $# -eq 0 ] 
+then
+  for f in resume_markdown/*.md; 
+    do pandoc "$f" -s -f markdown+yaml_metadata_block \
+    --template resume_template/resume_template.latex -o "${f%.md}.pdf"; 
+  done
+  echo "converted all markdown file";
+else
+  pandoc "resume_markdown/$1" -s -f markdown+yaml_metadata_block --template resume_template/resume_template.latex -o "resume_markdown/${1%.md}.pdf"; 
+  echo "converted $1";
+fi
 
 for f in resume_markdown/*.pdf; 
 do mv "$f" resume_pdf; 
 done 
+echo "done";
